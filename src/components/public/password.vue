3defd024e6139a5cd315e9f3b6c13074c3349de7
wyc123456 <1015354946@qq.com>
2020-02-15 07:49:02 +0800

    更新代码

--- src/components/public/password.vue
+++ src/components/public/password.vue
@@ -3,22 +3,30 @@
     title="修改用户密码"
     :visible="formShow"
     :before-close="handleClose"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false" append-to-body
     width="500px">
     <div class="dialog">
       <el-form  :model="formData" :rules="rules" ref="ruleForm" label-width="90px" size="small">
         <el-form-item label="原密码" prop="oldPassword">
-          <el-input type="password"
+          <el-input type="password"  show-password auto-complete="new-password"
                     v-model="formData.oldPassword"></el-input>
         </el-form-item>
 
-        <el-form-item label="新密码" prop="newPassword">
+        <el-form-item label="新密码" prop="password">
           <el-input type="password"
-                    v-model="formData.newPassword"></el-input>
+                    v-model="formData.password"></el-input>
         </el-form-item>
 
         <el-form-item label="确认密码" prop="confirmPassword">
           <el-input type="password"
                     v-model="formData.confirmPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" prop="captchaCode">
+           <div class="captchaCode">
+             <el-input v-model="formData.captchaCode"></el-input>
+             <img :src="imgSrc" alt="" @click="getCode">
+           </div>
         </el-form-item>
       </el-form>
     </div>
@@ -44,23 +52,36 @@
       var validatePass2 = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请再次输入密码'));
-        } else if (value !== this.formData.newPassword) {
+        } else if (value !== this.formData.password) {
           callback(new Error('两次输入密码不一致!'));
         } else {
           callback();
         }
       };
+      var captchaCode = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('验证码不能为空'));
+        } else {
+          callback();
+        }
+      }
       return {
         formData:{
           oldPassword:"",
-          newPassword:"",
-          confirmPassword:""
+          password:"",
+          confirmPassword:"",
+          captchaCode:"",
+          captchaId: "",
         },
         rules:{
           oldPassword:[{ required: true,message:'请输入原始密码'}],
-          newPassword:[{ required: true,validator:validatePass}],
-          confirmPassword:[{ required: true,validator:validatePass2}]
-        }
+          password:[{ required: true,validator:validatePass}],
+          confirmPassword:[{ required: true,validator:validatePass2}],
+          captchaCode: [
+            {validator: captchaCode, trigger: 'blur'}
+          ]
+        },
+        imgSrc:''
       }
     },
     props:{
@@ -72,8 +93,10 @@
       handleClose(){
         this.formData = {
           oldPassword:"",
-          newPassword:"",
-          confirmPassword:""
+          password:"",
+          confirmPassword:"",
+          captchaId:"",
+          captchaCode:"",
         };
         this.$emit("handleFormClose", "passwordChange");
       },
@@ -89,7 +112,7 @@
               background: 'rgba(0, 0, 0, 0.7)'
             });
             var self = this;
-            this.$axios.post(process.env.API_ACS + 'user/password/update',
+            this.$axios.post(process.env.API_BASE + 'user/changePassword',
               self.formData)
               .then(function (response) {
                 if(response.data.success){
@@ -110,8 +133,36 @@
             return false;
           }
         });
+      },
+      async getCode() {
+        await this.$axios.get(process.env.API_BASE + 'common/getCaptcha?date='+Date.now())
+          .then((response) => {
+            if (response.status=='200') {
+              this.imgSrc = response.data.base64Code;
+              this.formData.captchaId = response.data.key;
+            } else {
+              this.$message.error(response.data.msg);
+            }
+          })
+          .catch((error) => {
+            // console.log(error);
+          });
       }
+    },
+    async created() {
+      await this.getCode()
     }
   }
 </script>
+<style scoped>
+  .captchaCode{
+    display: flex;
+    align-items: center;
+  }
+  .captchaCode img{
+    width: 150px;
+    margin-left: 15px;
+    cursor: pointer;
+  }
+</style>
 
