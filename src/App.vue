57719b8f4814989324c1047140867bba281a8c94
wyc123456 <1015354946@qq.com>
2020-02-16 08:19:49 +0800

    更新代码

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
   body, html {
     height: 100%;
     overflow: hidden;
-    background: rgb(248,247,252);
+    background: rgb(248, 247, 252);
   }
 
 
@@ -56,33 +56,48 @@
     border-top: 1px solid #d5d5d5 !important;
     background: rgba(248, 248, 248, 1) !important;
   }
-  ul,li{
+
+  ul, li {
     list-style: none;
   }
-  .root{
+
+  .root {
     height: calc(100% - 110px);
     padding: 30px 20px;
-     box-sizing: border-box;
-    background:rgba(255,255,255,1);
-    box-shadow:0px 2px 6px 0px rgba(221,226,255,0.53);
+    box-sizing: border-box;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 6px 0px rgba(221, 226, 255, 0.53);
     margin: 0 30px;
   }
+
   .footer {
     height: 60px;
     text-align: center;
     font-size: 14px;
     color: #999;
   }
- .root .el-table::before{
+
+  .root .el-table::before {
     height: 0px !important;
   }
- .root table thead{
-   background-color: #F8F7FC !important;
+
+  .root table thead {
+    background-color: #F8F7FC !important;
   }
- .root table thead tr th{
-   background-color: #F8F7FC !important;
+
+  .root table thead tr th {
+    background-color: #F8F7FC !important;
   }
-  .el-dropdown{
+
+  .el-dropdown {
     cursor: pointer !important;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    -webkit-transition-delay: 99999s !important;
+    -webkit-transition: color 99999s ease-out, background-color 99999s ease-out !important;
   }
 </style>
